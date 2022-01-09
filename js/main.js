56690fe5518ce136189c9c653a074d0597fbab95
Marwan Sayed <mass2595@yahoo.com>
2022-01-09 08:44:01 +0200

    login & register

--- js/main.js
+++ js/main.js
@@ -1,57 +1,7 @@
-// window.addEventListener("load",
-//     function(){
-//         var calendarEl = document.getElementById('calendar');
-//         var calendar = new FullCalendar.Calendar(calendarEl, {
-//             headerToolbar: {
-//                 left: '',
-//                 center: 'title',
-//                 right: '',
-//             },
-//             editable: false,
-//             firstDay: 6, //  1(Monday) this can be changed to 0(Sunday) for the USA system
-//             selectable: false,
-//             defaultView: 'month',
-//             axisFormat: 'h:mm',
-//             columnFormat: {
-//                 month: 'ddd', // Mon 
-//                 agendaDay: 'dddd d'
-//             },
-//             eventOrder:false,
-//     allDaySlot: true,
-//     selectHelper: false,
-//             events: [
-//                 {
-//                     title: "Attended",
-//                     start: new Date(),
-//                     allDay: true,
-//                     backgroundColor: 'green',
-//                 },
-//                 {
-//                     title: "in : 9:30",
-//                     start: new Date(),
-//                     allDay: true,
-//                     backgroundColor:"blue",
-//                 },
-//                 {
-//                     title: "out : 5:05",
-//                     start: new Date(),
-//                     allDay: true,
-//                     backgroundColor:"blue",
-//                 },
-//                 {
-//                     title: "Absent",
-//                     start: new Date(),
-//                     allDay: true,
-//                     backgroundColor:"red",
-//                 },
-//                 {
-//                     title: "Excuse out 3:00",
-//                     start: new Date(),
-//                     allDay: true,
-//                     backgroundColor:"purple",
-//                 },
-//             ],
-//         });
-//         calendar.render();
-//     }
-// );
+$(function(){
+    let users = [{},{},{},{}];
+    let attendence = [{},{},{},{}];
+    localStorage.setItem("users",users);
+    localStorage.setItem("attendence",attendence);
+    localStorage.setItem("logged",false);
+});
